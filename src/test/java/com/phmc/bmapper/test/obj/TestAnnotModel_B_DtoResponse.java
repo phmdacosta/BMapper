55a5ee6872989d416e348d1feb4927af90885e99
phmdacosta <ph.mcosta@hotmail.com>
2023-08-15 15:54:30 +0100

    BM-0007
    Create new test cases for annotation mapping

---
+++ src/test/java/com/phmc/bmapper/test/obj/TestAnnotModel_B_DtoResponse.java
@@ -0,0 +1,7 @@
+package com.phmc.bmapper.test.obj;
+
+import com.phmc.bmapper.annotation.MappingClass;
+
+@MappingClass(targetClass = TestAnnotModel_B.class)
+public class TestAnnotModel_B_DtoResponse extends TestAnnotModel_B_Dto {
+}
