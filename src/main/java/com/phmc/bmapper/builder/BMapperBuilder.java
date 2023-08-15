223d33e656f2d13bb4dafc8393eb741d97c28c3d
phmdacosta <ph.mcosta@hotmail.com>
2023-08-15 10:59:48 +0100

    BM-0005
    Improving builder

--- src/main/java/com/phmc/bmapper/builder/BMapperBuilder.java
+++ src/main/java/com/phmc/bmapper/builder/BMapperBuilder.java
@@ -5,33 +5,24 @@ import com.phmc.bmapper.utils.MappingContext;
 import com.phmc.bmapper.utils.MappingType;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * Component responsible for creating the BMapper, with which it is possible to configure the mapper.
+ */
 public class BMapperBuilder {
 
     private BMapperBuilderHelper helper;
     private boolean lazyMappings;
 
     // STATIC METHODS
-    public static BMapperBuilder init(final ApplicationContext context) {
-        BMapperBuilder builder = new BMapperBuilder();
-        builder.helper = builder.initHelper(context);
-        return builder;
-    }
 
-    public static BMapperBuilder init(final Class<?> mainClass) {
+    /**
+     * Initiate the builder.
+     *
+     * @return builder
+     */
+    public static BMapperBuilder init() {
         BMapperBuilder builder = new BMapperBuilder();
-        builder.helper = builder.initHelper(mainClass);
-        return builder;
-    }
-
-    public static BMapperBuilder init(final Package targetPackage) {
-        BMapperBuilder builder = new BMapperBuilder();
-        builder.helper = builder.initHelper(targetPackage);
-        return builder;
-    }
-
-    public static BMapperBuilder init(final MappingContext mappingContext) {
-        BMapperBuilder builder = new BMapperBuilder();
-        builder.helper = builder.initHelper(mappingContext);
+        builder.initHelper();
         return builder;
     }
 
@@ -42,21 +33,70 @@ public class BMapperBuilder {
     }
 
     // NOT STATIC METHODS
+
+    /**
+     * Enable mapping through XML files.
+     *
+     * @return builder
+     */
     public BMapperBuilder enableXmlMapping() {
         this.helper.getMappingDataBuilder().addMappingTypes(MappingType.XML);
         return this;
     }
 
-    public BMapperBuilder disableAnnotationMapping() {
-        this.helper.getMappingDataBuilder().removeMappingTypes(MappingType.ANNOTATION);
-        return this;
+    /**
+     * Enable mapping through Annotations on classes.
+     *
+     * @param context   Context to assist in searching for the annotated classes
+     * @return builder
+     */
+    public BMapperBuilder enableAnnotationMapping(final ApplicationContext context) {
+        MappingContext mappingContext = new MappingContext();
+        mappingContext.setContext(context);
+        return enableAnnotationMapping(mappingContext);
     }
 
+    /**
+     * Enable mapping through Annotations on classes.
+     *
+     * @param mainClass Project's main class ti extract the context to assist in searching for the annotated classes
+     * @return builder
+     */
+    public BMapperBuilder enableAnnotationMapping(final Class<?> mainClass) {
+        MappingContext mappingContext = new MappingContext();
+        mappingContext.setMainClass(mainClass);
+        return enableAnnotationMapping(mappingContext);
+    }
+
+    /**
+     * Enable mapping through Annotations on classes.
+     *
+     * @param targetPackage Annotated classes package to assist in searching
+     * @return builder
+     */
+    public BMapperBuilder enableAnnotationMapping(final Package targetPackage) {
+        MappingContext mappingContext = new MappingContext();
+        mappingContext.setTargetPackage(targetPackage);
+        return enableAnnotationMapping(mappingContext);
+    }
+
+    /**
+     * BMapper, by default, builds all mappings defined through annotations or XML files (if these are enabled). <br>
+     * This method serves to tell BMapper that it needs to load the mappings manually later. <br>
+     * To load the mappings manually use method {@linkplain BMapperBuilder#loadMappingProps(BMapper)}
+     *
+     * @return builder
+     */
     public BMapperBuilder lazyMappings() {
         this.lazyMappings = true;
         return this;
     }
 
+    /**
+     * Build BMapper object.
+     *
+     * @return New BMapper object
+     */
     public BMapper build() {
         if (!lazyMappings) {
             this.helper.getMappingDataBuilder().initMappingProps();
@@ -66,29 +106,20 @@ public class BMapperBuilder {
         return mapper;
     }
 
-    private BMapperBuilderHelper initHelper(final ApplicationContext context) {
-        MappingContext mappingContext = new MappingContext();
-        mappingContext.setContext(context);
-        return initHelper(mappingContext);
+    // PRIVATE METHODS
+
+    private void initHelper() {
+        if (this.helper == null) {
+            this.helper = new BMapperBuilderHelper();
+        }
+        if (this.helper.getMappingDataBuilder() == null) {
+            this.helper.setMappingDataBuilder(new MappingDataBuilder());
+        }
     }
 
-    private BMapperBuilderHelper initHelper(final Class<?> mainClass) {
-        MappingContext mappingContext = new MappingContext();
-        mappingContext.setMainClass(mainClass);
-        return initHelper(mappingContext);
-    }
-
-    private BMapperBuilderHelper initHelper(final Package targetPackage) {
-        MappingContext mappingContext = new MappingContext();
-        mappingContext.setTargetPackage(targetPackage);
-        return initHelper(mappingContext);
-    }
-
-    private BMapperBuilderHelper initHelper(final MappingContext mappingContext) {
-        BMapperBuilderHelper helper = new BMapperBuilderHelper();
-        helper.setMappingDataBuilder(new MappingDataBuilder());
-        helper.getMappingDataBuilder().setMappingContext(mappingContext);
+    private BMapperBuilder enableAnnotationMapping(final MappingContext mappingContext) {
+        this.helper.getMappingDataBuilder().setMappingContext(mappingContext);
         helper.getMappingDataBuilder().addMappingTypes(MappingType.ANNOTATION);
-        return helper;
+        return this;
     }
 }
