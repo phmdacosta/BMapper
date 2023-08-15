5ad9c35a3937634c102ae5decdafb04cd81ec2db
phmdacosta <ph.mcosta@hotmail.com>
2023-08-15 15:54:55 +0100

    Revert "BM-0006"
    This reverts commit 53c60195dc33d1ebd9c2e20f032522f93ca19514.

--- src/test/java/com/phmc/bmapper/test/obj/TestAnnotModel_B_DtoRequest.java
+++
@@ -1,7 +0,0 @@
-package com.phmc.bmapper.test.obj;
-
-import com.phmc.bmapper.annotation.MappingClass;
-
-@MappingClass(targetClass = TestAnnotModel_B.class)
-public class TestAnnotModel_B_DtoRequest extends TestAnnotModel_B_Dto {
-}
--- src/test/java/com/phmc/bmapper/test/obj/TestModelChildADtoRequest.java
+++
@@ -1,7 +0,0 @@
-package com.phmc.bmapper.test.obj;
-
-import com.phmc.bmapper.annotation.MappingClass;
-
-@MappingClass(targetClass = TestModelChildA.class)
-public class TestModelChildADtoRequest extends TestModelChildADto {
-}
